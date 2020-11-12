d0e5e0e3f1ea705aa90b5992973f6fe9c650588f
manchevatsveti <72023208+manchevatsveti@users.noreply.github.com>
2020-11-12 16:27:44 +0200

    Update BMI_tset.java

--- BMI_tset.java
+++ BMI_tset.java
@@ -3,8 +3,9 @@ package eddu.smg;
 public class BMI_tset {
 
 	public static void main(String[] args) {
-		BMI bmi1 = new BMI("Tom",16,80,1.85);
-		BMI bmi2 = new BMI("Sam", 65,1.50);
+		BMI bmi1 = new BMI("Tom",16,176.37,71.83);//80kg,185cm
+		BMI bmi2 = new BMI("Sam", 143.3,59.05);//65kg,150cm
+		BMI bmi3 = new BMI("Mark",19, 110.23,6,2.8);
 		System.out.println(bmi1.getName());
 		System.out.println(bmi1.getAge());
 		System.out.println(bmi1.getHeight());
@@ -18,6 +19,13 @@ public class BMI_tset {
 		System.out.println(bmi2.getWeight());
 		System.out.println(bmi2.getBMI());
 		System.out.println(bmi2.getStatus());
+		
+		System.out.println(bmi3.getName());
+		System.out.println(bmi3.getAge());
+		System.out.println(bmi3.getHeight());
+		System.out.println(bmi3.getWeight());
+		System.out.println(bmi3.getBMI());
+		System.out.println(bmi3.getStatus());
 	}
 
 }
