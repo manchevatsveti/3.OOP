90adb6e2fa9214d742c5b7187d1ea0603cb40144
manchevatsveti <72023208+manchevatsveti@users.noreply.github.com>
2020-11-26 21:36:55 +0200

    Update TestPerson.java

--- TestPerson.java
+++ TestPerson.java
@@ -4,11 +4,11 @@ public class TestPerson {
 
 	   public static void main(String[] args) {
 
-	        Person person = new Person("person");
-	        Student student = new Student("student");
-	        Employee employee = new Employee("employee");
-	        Faculty faculty = new Faculty("faculty");
-	        Staff staff = new Staff("staff");
+	        Person person = new Person("Tom", "Rakovski23", "0885678891", "tomm@abv.bg");
+	        Student student = new Student("Tom", "Rakovski23", "0885678891", "tomm@abv.bg", Student.FRESHMAN);
+	        Employee employee = new Employee("Tom", "Rakovski23", "0885678891", "tomm@abv.bg",4500,"Head Office");
+	        Faculty faculty = new Faculty ("Tom", "Rakovski23", "0885678891", "tomm@abv.bg","Head Office",4500,54,"boss");
+	        Staff staff = new Staff ("Tom", "Rakovski23", "0885678891", "tomm@abv.bg","Head Office",4500,"sanitor");
 
 	        System.out.println(person.toString());
 	        System.out.println(student.toString());
